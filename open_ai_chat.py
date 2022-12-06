1f4874fcebba5ebef1cf16d1f88bb8eaf15ece4e
Caspar Nettelbladt <caspar.n@gmail.com>
2022-12-06 05:20:50 +0100

    Move retry to decorator

--- open_ai_chat.py
+++ open_ai_chat.py
@@ -2,6 +2,7 @@ import json
 import logging
 import uuid
 from dataclasses import dataclass
+from functools import wraps
 
 import requests
 
@@ -44,11 +45,27 @@ class ConversationResponse:
         return self.message.content.parts[0]
 
 
+def request_retry(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        MAX_ATTEMPTS = 3
+        for attempt in range(1, MAX_ATTEMPTS + 1):
+            try:
+                return func(*args, **kwargs)
+            except requests.exceptions.RequestException as e:
+                if attempt == MAX_ATTEMPTS:
+                    raise e
+                LOGGER.warning(f"Attempt {attempt}/{MAX_ATTEMPTS} failed. Retrying...")
+
+    return wrapper
+
+
 class OpenAiChatClient:
     def __init__(self, session_token: str):
         self.session_token = session_token
         self._access_token = None
 
+    @request_retry
     def conversation(
         self,
         prompt: str,
@@ -68,56 +85,44 @@ class OpenAiChatClient:
             "parent_message_id": parent_message_id or str(generate_uuid()),
             "model": "text-davinci-002-render",
         }
+        headers = {
+            "Accept": "application/json",
+            "Authorization": "Bearer " + self.access_token(),
+            "Content-Type": "application/json",
+        }
+        response = requests.post(
+            "https://chat.openai.com/backend-api/conversation",
+            headers=headers,
+            data=json.dumps(data),
+        )
 
-        MAX_RETRIES = 3
-        for retry in range(MAX_RETRIES):
-            try:
-                headers = {
-                    "Accept": "application/json",
-                    "Authorization": "Bearer " + self.access_token(),
-                    "Content-Type": "application/json",
-                }
-                response = requests.post(
-                    "https://chat.openai.com/backend-api/conversation",
-                    headers=headers,
-                    data=json.dumps(data),
-                )
+        if response.status_code == 401:
+            LOGGER.debug("Access token expired. Refreshing...")
+            self._access_token = None
 
-                if response.status_code == 401:
-                    LOGGER.debug("Access token expired. Refreshing...")
-                    self._access_token = None
+        response.raise_for_status()
 
-                response.raise_for_status()
-
-                data = json.loads(response.text.splitlines()[-4][6:])
-                msg = data["message"]
-                return ConversationResponse(
-                    message=Message(
-                        id=msg["id"],
-                        role=msg["role"],
-                        user=msg["user"],
-                        create_time=msg["create_time"],
-                        update_time=msg["update_time"],
-                        content=Content(
-                            content_type=msg["content"]["content_type"],
-                            parts=msg["content"]["parts"],
-                        ),
-                        end_turn=msg["end_turn"],
-                        weight=msg["weight"],
-                        metadata=msg["metadata"],
-                        recipient=msg["recipient"],
-                    ),
-                    conversation_id=data["conversation_id"],
-                    error=data["error"],
-                )
-            except requests.exceptions.RequestException as e:
-                LOGGER.warning(
-                    f"Request failed. Retrying... ({retry + 1}/{MAX_RETRIES})"
-                )
-                if retry == MAX_RETRIES - 1:
-                    raise e
-
-        raise RuntimeError("Should not reach here")
+        data = json.loads(response.text.splitlines()[-4][6:])
+        msg = data["message"]
+        return ConversationResponse(
+            message=Message(
+                id=msg["id"],
+                role=msg["role"],
+                user=msg["user"],
+                create_time=msg["create_time"],
+                update_time=msg["update_time"],
+                content=Content(
+                    content_type=msg["content"]["content_type"],
+                    parts=msg["content"]["parts"],
+                ),
+                end_turn=msg["end_turn"],
+                weight=msg["weight"],
+                metadata=msg["metadata"],
+                recipient=msg["recipient"],
+            ),
+            conversation_id=data["conversation_id"],
+            error=data["error"],
+        )
 
     def access_token(self):
         if not self._access_token:
