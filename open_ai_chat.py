e9462a50a38fa29ccbda1845790cf464cd36c53c
Caspar Nettelbladt <caspar.n@gmail.com>
2022-12-06 04:38:11 +0100

    Add retry policy

--- open_ai_chat.py
+++ open_ai_chat.py
@@ -93,42 +93,53 @@ class OpenAiChatClient:
             "parent_message_id": conversation.parent_message_id,
             "model": "text-davinci-002-render",
         }
-        headers = {
-            "Accept": "application/json",
-            "Authorization": "Bearer " + self.access_token,
-            "Content-Type": "application/json",
-        }
-        response = requests.post(
-            "https://chat.openai.com/backend-api/conversation",
-            headers=headers,
-            data=json.dumps(data),
-        )
-        # Unauthorized
-        if response.status_code == 401:
-            self._access_token = None
-            # TODO: retry
-            raise ValueError("Unauthorized")
 
-        response.raise_for_status()
+        MAX_RETRIES = 3
+        for retry in range(MAX_RETRIES):
+            try:
+                headers = {
+                    "Accept": "application/json",
+                    "Authorization": "Bearer " + self.access_token,
+                    "Content-Type": "application/json",
+                }
+                response = requests.post(
+                    "https://chat.openai.com/backend-api/conversation",
+                    headers=headers,
+                    data=json.dumps(data),
+                )
 
-        data = json.loads(response.text.splitlines()[-4][6:])
-        msg = data["message"]
-        return ConversationResponse(
-            message=Message(
-                id=msg["id"],
-                role=msg["role"],
-                user=msg["user"],
-                create_time=msg["create_time"],
-                update_time=msg["update_time"],
-                content=Content(
-                    content_type=msg["content"]["content_type"],
-                    parts=msg["content"]["parts"],
-                ),
-                end_turn=msg["end_turn"],
-                weight=msg["weight"],
-                metadata=msg["metadata"],
-                recipient=msg["recipient"],
-            ),
-            conversation_id=data["conversation_id"],
-            error=data["error"],
-        )
+                if response.status_code == 401:
+                    LOGGER.debug("Access token expired. Refreshing...")
+                    self._access_token = None
+
+                response.raise_for_status()
+
+                data = json.loads(response.text.splitlines()[-4][6:])
+                msg = data["message"]
+                return ConversationResponse(
+                    message=Message(
+                        id=msg["id"],
+                        role=msg["role"],
+                        user=msg["user"],
+                        create_time=msg["create_time"],
+                        update_time=msg["update_time"],
+                        content=Content(
+                            content_type=msg["content"]["content_type"],
+                            parts=msg["content"]["parts"],
+                        ),
+                        end_turn=msg["end_turn"],
+                        weight=msg["weight"],
+                        metadata=msg["metadata"],
+                        recipient=msg["recipient"],
+                    ),
+                    conversation_id=data["conversation_id"],
+                    error=data["error"],
+                )
+            except requests.exceptions.RequestException as e:
+                LOGGER.warning(
+                    f"Request failed. Retrying... ({retry + 1}/{MAX_RETRIES})"
+                )
+                if retry == MAX_RETRIES - 1:
+                    raise e
+
+        raise RuntimeError("Should not reach here")
